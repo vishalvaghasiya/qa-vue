9d3a94e7eea9c693ff6ab09281d48f21ac73264f
vishal vaghasiya <vishalvaghasiya.me@gmail.com>
2020-08-14 17:23:06 +0530

    Router uisng component call Add Router-view

--- src/router/index.js
+++ src/router/index.js
@@ -1,29 +1,37 @@
 import Vue from 'vue'
 import VueRouter from 'vue-router'
 import Home from '../views/Home.vue'
+import vish from "../views/vish";
+
 
 Vue.use(VueRouter)
 
-  const routes = [
-  {
-    path: '/',
-    name: 'Home',
-    component: Home
-  },
-  {
-    path: '/about',
-    name: 'About',
-    // route level code-splitting
-    // this generates a separate chunk (about.[hash].js) for this route
-    // which is lazy-loaded when the route is visited.
-    component: () => import(/* webpackChunkName: "about" */ '../views/About.vue')
-  }
+const routes = [
+    {
+        path: '/',
+        name: 'Home',
+        component: Home
+    },
+    {
+        path: '/vish',
+        name: 'vish',
+        component: vish
+    },
+    /*{
+        path: '/about',
+        name: 'About',
+        component: About
+        // route level code-splitting
+        // this generates a separate chunk (about.[hash].js) for this route
+        // which is lazy-loaded when the route is visited.
+        // component: () => import(/!* webpackChunkName: "about" *!/ '../views/About.vue')
+    }*/
 ]
 
 const router = new VueRouter({
-  mode: 'history',
-  base: process.env.BASE_URL,
-  routes
+    mode: 'history',
+    base: process.env.BASE_URL,
+    routes
 })
 
 export default router
